27f0319f4ba366e656267e9d16d7f0a8e487cd37
Shirley Li <shirleyy.li@outlook.com>
2024-06-29 18:16:20 -0400

    primary code for generator and discriminator

---
+++ discriminator.py
@@ -0,0 +1,99 @@
+# importing libraries
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torchaudio
+import torchvision
+import os
+from torchvision import transforms, datasets
+import numpy as np
+import shutil
+from torch.utils.data import DataLoader, Subset
+import torch.optim as optim
+import time
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR
+
+# ReLU Block 
+
+class ConvInstanceNormLeakyReLUBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int):
+        """
+        Class object initialization for Convolution-InstanceNorm-LeakyReLU layer
+
+        We use leaky ReLUs with a slope of 0.2.
+        """
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=4,
+                stride=stride,
+                padding=1,
+                bias=True,
+                padding_mode="reflect",
+            ),
+            nn.InstanceNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
+        """
+        Let Ck denote a 4 × 4 Convolution-InstanceNorm-LeakyReLU layer with 
+        k filters and stride 2. Discriminator architecture is: C64-C128-C256-C512. 
+        
+        After the last layer, we apply a convolution to produce a 1-dimensional 
+        output. 
+        
+        We use leaky ReLUs with a slope of 0.2.
+        """
+        super().__init__()
+        self.initial_layer = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                features[0],
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                padding_mode="reflect",
+            ),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                ConvInstanceNormLeakyReLUBlock(
+                    in_channels, 
+                    feature, 
+                    stride=1 if feature == features[-1] else 2,
+                )
+            )
+            in_channels = feature
+
+        # After the last layer, we apply a convolution to produce a 1-dimensional output 
+        layers.append(
+            nn.Conv2d(
+                in_channels,
+                1,
+                kernel_size=4,
+                stride=1,
+                padding=1,
+                padding_mode="reflect",
+            )
+        )
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.initial_layer(x)
+
+        # feed the model output into a sigmoid function to make a 1/0 label
+        return torch.sigmoid(self.model(x))
